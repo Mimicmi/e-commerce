9730d5120e5a521fd460e771ec4fd8804378dadf
unknown <simon.frezard@gmail.com>
2021-03-23 16:26:49 +0100

    modified Users database to have field is_admin, checked how php artisan migrate:refresh works, checked how php artisan db:seed works

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Subcategory;
+use App\Models\Product;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'SimonAdmin',
+            'email' => 'simon@admin.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => NOW(),
+            'address' => 'France',
+            'phone_number' => '0601020304',
+            'is_admin' => 1
+        ]);
+        User::create([
+            'name' => 'JohnDoe',
+            'email' => 'john@doe.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => NOW(),
+            'address' => 'America',
+            'phone_number' => '0609080706',
+            'is_admin' => 0
+        ]);
     }
 }
